95d81401a0b149bebf9df707d8182b1124ae92b7
nottoday95z <nottoday95z@gmail.com>
2023-01-06 16:42:44 +0300

    tests ref

--- todolist/goals/urls.py
+++ todolist/goals/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 
-from . import views
+from goals import views
+
 
 urlpatterns = [
-    path('goal_category/create', views.GoalCategoryCreateView.as_view()),
-    path('goal_category/list', views.GoalCategoryListView.as_view()),
-    path('goal_category/<pk>', views.GoalCategoryView.as_view()),
-    path('goal/create', views.GoalCreateView.as_view()),
-    path('goal/list', views.GoalListView.as_view()),
-    path('goal/<pk>', views.GoalView.as_view()),
-    path('goal_comment/create', views.GoalCommentCreateView.as_view()),
-    path('goal_comment/list', views.GoalCommentListView.as_view()),
-    path('goal_comment/<pk>', views.GoalCommentView.as_view()),
-    path('board/create', views.BoardCreateView.as_view()),
-    path('board/list', views.BoardListView.as_view()),
-    path('board/<pk>', views.BoardView.as_view()),
+    path("goal_category/create", views.GoalCategoryCreateView.as_view(), name="goal_category_create"),
+    path("goal_category/list", views.GoalCategoryListView.as_view(), name="goal_category_list"),
+    path("goal_category/<pk>", views.GoalCategoryView.as_view(), name="goal_category_pk"),
+    path("goal/create", views.GoalCreateView.as_view(), name="goal_create"),
+    path("goal/list", views.GoalListView.as_view(), name="goal_list"),
+    path("goal/<pk>", views.GoalView.as_view(), name="goal_pk"),
+    path("goal_comment/create", views.GoalCommentCreateView.as_view(), name="goal_comment_create"),
+    path("goal_comment/list", views.GoalCommentListView.as_view(), name="goal_comment_list"),
+    path("goal_comment/<pk>", views.GoalCommentView.as_view(), name="goal_comment_pk"),
+    path("board/create", views.BoardCreateView.as_view(), name="board_create"),
+    path("board/list", views.BoardListView.as_view(), name="board_list"),
+    path("board/<pk>", views.BoardView.as_view(), name="board_pk"),
 ]
--- todolist/tests/board_test.py
+++ todolist/tests/board_test.py
@@ -13,17 +13,17 @@ from tests.factories import BoardFactory, BoardParticipantFactory
 @pytest.mark.django_db
 def test_create(auth_user: APIClient, board: Board) -> None:
     response = auth_user.post(
-        reverse('create_board'),
+        reverse("board_create"),
         data={
-            'title': 'test board',
+            "title": "test board",
         },
     )
     expected_response = {
-        'id': response.data.get('id'),
-        'title': 'test board',
-        'created': response.data.get('created'),
-        'updated': response.data.get('updated'),
-        'is_deleted': False,
+        "id": response.data.get("id"),
+        "title": "test board",
+        "created": response.data.get("created"),
+        "updated": response.data.get("updated"),
+        "is_deleted": False,
     }
 
     assert response.status_code == 201
@@ -32,14 +32,14 @@ def test_create(auth_user: APIClient, board: Board) -> None:
 
 @pytest.mark.django_db
 def test_list(auth_user: APIClient, board: Board, add_user: User) -> None:
-    board_ = BoardFactory.create(title='test board')
+    board_ = BoardFactory.create(title="test board")
     participant = BoardParticipantFactory.create(board=board_, user=add_user)
-    response = auth_user.get(f"{reverse('list_board')}?limit=1")
+    response = auth_user.get(f"{reverse('board_list')}?limit=1")
     expected_response = {
-        'count': 1,
-        'next': None,
-        'previous': None,
-        'results': BoardListSerializer(instance=(board_,), many=True).data
+        "count": 1,
+        "next": None,
+        "previous": None,
+        "results": BoardListSerializer(instance=(board_,), many=True).data
     }
 
     assert response.status_code == 200
@@ -48,7 +48,7 @@ def test_list(auth_user: APIClient, board: Board, add_user: User) -> None:
 
 @pytest.mark.django_db
 def test_retrieve(auth_user: APIClient, board: Board, add_user: User, board_participant: BoardParticipant) -> None:
-    response = auth_user.get(reverse('retrieve_board', args=[board.pk]))
+    response = auth_user.get(reverse("board_pk", args=[board.pk]))
 
     expected_response = BoardSerializer(instance=board).data
 
@@ -58,7 +58,7 @@ def test_retrieve(auth_user: APIClient, board: Board, add_user: User, board_part
 
 @pytest.mark.django_db
 def test_delete(auth_user: APIClient, board: Board, add_user: User, board_participant: BoardParticipant) -> None:
-    response = auth_user.delete(reverse('retrieve_board', args=[board.pk]))
+    response = auth_user.delete(reverse("board_pk", args=[board.pk]))
 
     assert response.status_code == 204
 
@@ -66,13 +66,13 @@ def test_delete(auth_user: APIClient, board: Board, add_user: User, board_partic
 @pytest.mark.django_db
 def test_update(auth_user: APIClient, board: Board, add_user: User, board_participant: BoardParticipant) -> None:
     response = auth_user.put(
-        reverse('retrieve_board', args=[board.pk]),
+        reverse("board_pk", args=[board.pk]),
         data=json.dumps({
-            'title': 'put board',
-            'participants': [],
+            "title": "put board",
+            "participants": [],
         }),
-        content_type='application/json'
+        content_type="application/json"
     )
 
     assert response.status_code == 200
-    assert response.data.get('title') == 'put board'
+    assert response.data.get("title") == "put board"
